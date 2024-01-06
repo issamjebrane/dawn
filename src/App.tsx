8639109ac323552dcc1e87a111a029855d9924b9
issam <issamjebrane65@gmail>
2024-01-06 23:55:52 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import dawnImage from "../src/assets/dawn.png"
+import { FaFacebook } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa";
+
+import './App.css'
+
+function App() {
+    const emailAddress = 'contact.dawnstore@gmail.com';
+  
+    const handleClick = () => {
+      window.location.href = `mailto:${emailAddress}`;
+    };
+  return (
+    <>
+    <div className="container">
+      <div className="image">
+        <img src={dawnImage} alt="" />
+      </div>
+      <div className="subContainer">
+        <div>
+          <h1 className="title">Coming Soon</h1>
+          <h6 className="subtitle">
+            In the meantime, Sign up for our monthly
+            newsletter to stay up to date
+          </h6>
+          <div className="buttons">
+            <input type="text" placeholder="Email Address"/>
+            <button className="signButton">Sign Up</button>
+          </div>
+        </div>
+        <div className="links">
+          <a href="https://www.instagram.com/dawn.sunglasses/">
+            <FaInstagram/>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61555525734042">          
+            <FaFacebook/>
+          </a>
+          <a href="mailto:contact.dawnstore@gmail.com">
+            <BiLogoGmail/>
+          </a>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
